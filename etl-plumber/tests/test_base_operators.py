f88c1aa94ec2d75efc95f04af685edf58511365c
Janis Cakstins <janis.cakstins@slalom.com>
2022-04-07 20:29:32 +0000

    Added right shift test for extract transform pattern

--- etl-plumber/tests/test_base_operators.py
+++ etl-plumber/tests/test_base_operators.py
@@ -7,5 +7,7 @@ class TestRightShift():
     def test_extract_transform(self, 
                                dummy_extractor: ExtractOperator, 
                                dummy_transformer: TransformOperator):
-        dag = dummy_extractor >> dummy_transformer
-        assert isinstance(dag, DataFrame)
+        dag_single = dummy_extractor >> dummy_transformer
+        dag_multi = [dummy_extractor, dummy_extractor] >> dummy_transformer
+        assert isinstance(dag_single, DataFrame)
+        assert isinstance(dag_multi, DataFrame)
